0fda4dc0f349e1729d47420d35b1de248eed9f17
AnzhelikaWil <anzhelika.wilson@gmail.com>
2020-02-22 16:27:54 -0500

    added info

--- src/test/java/com/bookit/runners/CukesRunner.java
+++ src/test/java/com/bookit/runners/CukesRunner.java
@@ -1,4 +1,21 @@
 package com.bookit.runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        plugin = {
+                "json:target/cucumber.json"
+        },
+        features="src/test/resources/features",
+        glue="com/bookit/step_definitions",
+        dryRun = false,
+        tags="api"
+)
 public class CukesRunner {
+
+
 }
